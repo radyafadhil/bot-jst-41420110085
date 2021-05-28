299f3f3b4457183103e7f2cf61e30931666c0b44
radyafadhil <84943285+radyafadhil@users.noreply.github.com>
2021-05-28 10:57:49 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -34,7 +34,7 @@ bot.onText(/\/predict/, (msg) => {
 bot.on('message', (msg) => {
 	if(state == 1){
 		console.log(msg.Text);
-		s = msg.Text.split("|");
+		s = msg.text.split("|");
 		i = s[0]
 		v = s[1]
 		model.predict(
@@ -45,18 +45,17 @@ bot.on('message', (msg) => {
 		).then((jres)=>{
 			bot.sendMessage(
 				msg.chat.id,
-				'nilai v yang diprediksi adalah ${jres[0]} volt'
+				`nilai v yang diprediksi adalah ${jres[0]} volt`
         );
         bot.sendMessage(
-                        msg.chat.id,
-                        'nilai p yang diprediksi adalah ${jres[1]} watt'
+                msg.chat.id,
+                `nilai p yang diprediksi adalah ${jres[1]} watt`
         );
     })
     }else {
         state = 0 
         }
-    })
-
+})
 
 // routers
 r.get('/prediction/:i/:r', function(req, res, next) {    
