24694693c7a7a03df30dbb04b299a8de6dfa1e07
radyafadhil <84943285+radyafadhil@users.noreply.github.com>
2021-05-28 11:25:20 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -31,11 +31,37 @@ bot.onText(/\/predict/, (msg) => {
     state = 1
 });
 
+//bot.on('message', (msg) => {
+//	if(state == 1){
+//		console.log(msg.Text);		
+//  	}else {
+//        	state = 0 
+//        }
+//})
+
 bot.on('message', (msg) => {
 	if(state == 1){
-		console.log(msg.Text);		
-    	}else {
-        	state = 0 
+		console.log(msg.Text);
+		s = msg.text.split("|");
+		i = s[0]
+		v = s[1]
+		model.predict(
+			[
+				parseFloat(s[0]),
+				parseFloat(s[1])
+			]
+		).then((jres)=>{
+			bot.sendMessage(
+				msg.chat.id,
+				`nilai v yang diprediksi adalah ${jres[0]} volt`
+        );
+        bot.sendMessage(
+                msg.chat.id,
+                `nilai p yang diprediksi adalah ${jres[1]} watt`
+        );
+    })
+    }else {
+        state = 0 
         }
 })
 
